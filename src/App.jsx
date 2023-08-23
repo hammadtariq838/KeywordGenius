b96a759eae8d4523981285765f45f4dc5fcd6f05
hammadtariq838 <24100267@lums.edu.pk>
2023-08-23 12:36:26 +0500

    Updated UI

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ const App = () => {
   };
 
   return (
-    <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
+    <Box bg='blue.600' color='white' height='100vh' paddingTop={130} width='100vw'>
       <Container maxW='3xl' centerContent>
         <Header />
         <TextInput extractKeywords={extractKeywords} />
